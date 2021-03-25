492ca1a1eb3b18515eca1ea7831ec09ef70681c7
Dennis David Arango Mendieta <dennis@MacBook-Pro-de-Dennis.local>
2021-03-24 19:46:09 -0500

    Finished project

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import UserList from './screens/UserList'
+import CreateUserScreen from './screens/CreateUserScreen'
+import UserDetailScreen from './screens/UserDetailScreen'
+
+
+const  Stack = createStackNavigator()
+
+function MyStack(){
+  return (
+  <Stack.Navigator>
+    <Stack.Screen name="UserList" component={UserList} options={{title: 'Lista de Usuarios'}}/>
+    <Stack.Screen name="CreateUserScreen" component={CreateUserScreen} options={{title: 'Crear un nuevo usuario'}}/>
+    <Stack.Screen name="UserDetailScreen" component={UserDetailScreen} options={{title: 'Detalle de Usuario'}}/>
+  </Stack.Navigator>
+
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
   },
+  text:{
+    fontSize: 40
+  }
 });
